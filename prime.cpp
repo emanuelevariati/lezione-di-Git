2a1f7f5ba93305339e8a12fc943cfc0484b59070
emanuelevariati <emanuele.variati@studenti.unitn.it>
2020-03-25 11:56:44 +0100

    Change input type

--- prime.cpp
+++ prime.cpp
@@ -5,30 +5,30 @@
 using namespace std;
 
 bool isPrimeFor(int n){ //Definition
-    if (n%2==0)
-        return n==2;
+    if (n % 2 == 0)
+        return n == 2;
 
     int limit = sqrt(abs(n));
-    if (limit*limit==n)
+    if (limit*limit == n)
         return false;
 
     for (int i = 3; i <= sqrt(n); i+=2)
-        if (n%i==0)
+        if (n % i == 0)
             return false;
 
     return true;
 }
 
-bool isPrimeWhile(int n) { //Definition
-    if (n % 2 == 0)
+bool isPrimeWhile(int m) { //Definition
+    if (m % 2 == 0)
 	    return n ==2;
 	    
-    int limit = sqrt(abs(n));
-	if (limit * limit ==n)
+    int limit = sqrt(abs(m));
+	if (limit * limit == m)
 	    return false;
 		
 	int p = 3;
-	while (p <= limit && n% p !=0)
+	while (p <= limit && m % p != 0)
 		p += 2;
 		
 	return p > limit;
